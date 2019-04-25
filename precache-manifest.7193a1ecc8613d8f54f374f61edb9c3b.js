a2d039b8c5f0b64eb697b227a8abe7da6c73fa0c
Josh Bowden <git@josh.tf>
2019-04-25 19:26:28 +1000

    Updates

---
+++ precache-manifest.7193a1ecc8613d8f54f374f61edb9c3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f88703a1b1ab87ccaea256987870e6b",
+    "url": "/react-tictactoe/index.html"
+  },
+  {
+    "revision": "ab35ed90b66d0e9b54e8",
+    "url": "/react-tictactoe/static/css/main.1f8055d2.chunk.css"
+  },
+  {
+    "revision": "512adc3116ebd3fc275e",
+    "url": "/react-tictactoe/static/js/2.70aab571.chunk.js"
+  },
+  {
+    "revision": "ab35ed90b66d0e9b54e8",
+    "url": "/react-tictactoe/static/js/main.3612ebf9.chunk.js"
+  },
+  {
+    "revision": "32b32cc97a4c7174c6c0",
+    "url": "/react-tictactoe/static/js/runtime~main.f8e643ec.js"
+  }
+]);
